b6d7ff1d0104638a34481f79e0c36cb1ec9318c6
sseung-318 <woosung3690@gmail.com>
2022-08-02 17:56:52 +0900

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>hi</h1>
+    <hr />
+    <card-ui :menuTitle="'test title'" :menuCount="2" :backgroundColor="'red'" />
+    <card-ui />
+    <card-ui :menuTitle="'test title'" :menuCount="2" :backgroundColor="'red'" />
+    <card-ui />
+    <card-ui />
+    <card-ui />
+    <card-ui :menuCount="5"/>
+    <card-ui />
+    <card-ui :menuTitle="'other title'" />
+    <card-ui />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// define > 정의
+import CardUi from './components/card-ui.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { CardUi },
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/card-ui.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="amuz-card">
+    <h2 v-if="menuTitle">
+        {{ menuTitle }}
+    </h2>
+    <p v-else class="untitled">
+        no title
+    </p>
+    <ul>
+        <li>menu 1</li>
+        <li>menu 2</li>
+        <li>menu 3</li>
+        <li>menu 4</li>
+        <li>menu 5</li>
+        <li>menu 6</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props : {
+    menuTitle : String,
+    menuCount : Number,
+    backgroundcolor : String,
+  },
+}
+</script>
+
+<style>
+
+</style>
